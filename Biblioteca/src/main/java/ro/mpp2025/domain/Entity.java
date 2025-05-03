dbedf72cbf2d385a9569f711227fd436080ea877
elaa10 <alexandraisa17@yahoo.com>
2025-05-03 15:04:39 +0300

    verificare

---
+++ Biblioteca/src/main/java/ro/mpp2025/domain/Entity.java
@@ -0,0 +1,18 @@
+package ro.mpp2025.domain;
+
+import java.io.Serializable;
+
+public class Entity<ID> implements Serializable {
+
+    public Entity(){}
+
+    protected ID ID;
+
+    public ID getID() {
+        return ID;
+    }
+
+    public void setID(ID ID) {
+        this.ID = ID;
+    }
+}
